6290e39e2e9f83884c6a260b21d52185307b5c58
Rishi Ghosh <rishi4d@users.noreply.github.com>
2020-08-28 00:54:19 +0530

    Fixed Navbar

---
+++ app.js
@@ -0,0 +1,165 @@
+// Typing Animation & Blinking Cursor
+
+var i = 0, j = 0;
+var title = 'Electron Inc.';
+var cursor = true;
+var arr = new Array();
+
+function autotyper() {
+  if( i < title.length + 1 ){
+
+    if(i==0){
+      arr[j] = document.getElementById("name").innerHTML;
+      arr.splice(0, 0, title.charAt(i));
+      document.getElementById("name").innerHTML = arr[i];
+      document.getElementById("name").innerHTML = arr.join("");
+      j=0;
+    }
+    else{
+      arr.splice(j, 0, title.charAt(i));
+      document.getElementById("name").innerHTML = arr.join("");
+    }
+
+    i++;
+    j++;
+    setTimeout(autotyper, 70);
+  }
+}
+
+setTimeout(autotyper, 1550);
+
+// Cursor Blinking
+
+setInterval(() => {
+  if(cursor) {
+    document.getElementById('cursor').style.opacity = 0;
+    cursor = false;
+  }else {
+    document.getElementById('cursor').style.opacity = 1;
+    cursor = true;
+  }
+}, 500);
+
+
+
+// Particles
+particlesJS('particles-js',
+  
+  {
+    "particles": {
+      "number": {
+        "value": 60,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 3,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 190,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200
+        },
+        "push": {
+          "particles_nb": 3
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_color": "#b61924",
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
+    }
+  }
+
+);
